cd94a2061393d3f408ee7cd89b485f53d23c3b9d
Eduardo Aparecido <aparecidoj.edu@gmail.com>
2025-10-27 23:39:40 -0300

    Excluindo algumas categorias

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -2,10 +2,9 @@
 export const categories = [
   { id: 1, name: "Barzinho", icon: "🍻" },
   { id: 2, name: "Música ao Vivo", icon: "🎶" },
-  { id: 3, name: "Culinária Japonesa", icon: "🍱" },
+  { id: 3, name: "Restaurantes", icon: "🍱" },
   { id: 4, name: "Parques", icon: "🏃🏼‍♂️" },
   { id: 5, name: "Shoppings", icon: "🏢" },
-  { id: 6, name: "Cinema", icon: "🎬" },
-  { id: 7, name: "Teatro", icon: "🎭" },
-  { id: 8, name: "Restaurantes", icon: "🥗" },
+  { id: 6, name: "Teatro", icon: "🎭" },
+  { id: 7, name: "Culinária Japonesa", icon: "🥗" },
 ];
